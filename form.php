78a7fe4378f25e23c370cb5dbe53a50d7993e86d
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-03-30 19:39:19 +0200

    feat: fixed form

--- form.php
+++ form.php
@@ -9,92 +9,91 @@ if (!is_logged_in()) {
 
 include "_header.php";
 
-$query = $mysqli->prepare("SELECT * FROM courses");
-$query->execute();
-$courses = $query->get_result()->fetch_assoc();
-
 if (isset($_GET['course'], $_GET['exercice'], $_GET['language'], $_GET['file'])) {
-    // TODO: lancer le script de vérification
+    $query = $mysqli->prepare("INSERT INTO submissions (user_id, course_id, exercice_id, language, status) VALUES (?, ?, ?, ?, ?, ?)");
+    $query->bind_param("iiiss", $_SESSION['user']['id'], $_GET['course'], $_GET['exercice'], $_GET['language'], "pending");
+
+    if (!$query->execute()) return;
+
+    exit;
 }
 
 if (isset($_GET['course'])) {
+    $query = $mysqli->prepare("SELECT * FROM courses WHERE id = ?");
+    $query->bind_param("i", $_GET['course']);
+    $query->execute();
+    $course = $query->get_result()->fetch_assoc();
+
     $query = $mysqli->prepare("SELECT * FROM exercices WHERE course_id = ?");
     $query->bind_param("i", $_GET['course']);
     $query->execute();
-    $exercices = $query->get_result()->fetch_assoc();
+    $exercices = $query->get_result()->fetch_all(MYSQLI_ASSOC);
+} else {
+    $query = $mysqli->prepare("SELECT * FROM courses");
+    $query->execute();
+    $courses = $query->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 ?>
 
 <div class="w-100 h-100 d-flex align-items-center justify-content-center">
-    <div class="card">
-        <div class="card-header">Vérifiez votre code</div>
-        <div class="card-body p-4">
-            <?php if ($courses): ?>
-                <?php if (isset($_GET['course'], $exercices)): ?>
-                    <?php if (isset($_GET['exercice'])): ?>
-                        <?php if (isset($_GET['language'])): ?>
-                            <form class="" method="get">
-                                <input type="hidden" name="course" value="<?= $_GET['course'] ?>">
-                                <input type="hidden" name="exercice" value="<?= $_GET['exercice'] ?>">
-                                <input type="hidden" name="language" value="<?= $_GET['language'] ?>">
-
-                                <div class="input-group mb-3">
-                                    <input type="file" name="file" class="form-control" id="file">
-                                </div>
-
-                                <input type="submit" class="btn btn-primary rounded col-12" value="Envoyer mon fichier">
-                            </form>
-                        <?php else: ?>
-                            <form class="" method="get">
-                                <input type="hidden" name="course" value="<?= $_GET['course'] ?>">
-                                <input type="hidden" name="exercice" value="<?= $_GET['exercice'] ?>">
-
-                                <div class="input-group mb-3">
-                                    <label class="input-group-text" for="language"></label>
-                                    <select class="form-select" id="language">
-                                        <option selected>Choisir un language...</option>
-                                        <option value="C">C</option>
-                                        <option value="Python">Python</option>
-                                    </select>
-                                </div>
-
-                                <input type="submit" class="btn btn-primary rounded col-12" value="Choisir ce language">
-                            </form>
-                        <?php endif; ?>
-                    <?php else: ?>
-                        <form class="" method="get">
-                            <input type="hidden" name="course" value="<?= $_GET['course'] ?>">
-
-                            <div class="input-group mb-3">
-                                <label class="input-group-text" for="exercice"></label>
-                                <select class="form-select" name="exercice" id="exercice">
-                                    <option selected>Choisir un execice...</option>
-                                </select>
-                            </div>
-
-                            <input type="submit" class="btn btn-primary rounded col-12" value="Choisir cet exercice">
-                        </form>
-                    <?php endif; ?>
+    <?php if ($_GET['course']): ?>
+        <div class="card">
+            <div class="card-body">Choisir un cours</div>
+            <div class="card-body">
+                <?php if ($course): ?>
+                    <?php foreach ($courses as $course): ?>
+                        <a href="form.php?course=<?= $course['id'] ?>" title="<?= $course['description'] ?>"><?= $course['name'] ?></a>
+                    <?php endforeach; ?>
                 <?php else: ?>
+                    <div class="d-flex align-items-center text-danger gap-2">
+                        <i class="bi bi-exclamation-triangle-fill"></i>
+                        <p class="m-0">Problème lors du chargement des cours.</p>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    <?php else: ?>
+        <div class="card">
+            <div class="card-body">Soumettre votre fichier</div>
+            <div class="card-body">
+                <?php if ($course): ?>
                     <form class="" method="get">
+                        <input type="hidden" name="course" value="<?= $_GET['course'] ?>">
+
                         <div class="input-group mb-3">
-                            <label class="input-group-text" for="course"></label>
-                            <select class="form-select" name="course" id="course">
-                                <option selected>Choisir un cours...</option>
+                            <label class="input-group-text" for="exercice"></label>
+                            <select class="form-select" name="exercice" id="exercice">
+                                <option selected>Choisir un execice...</option>
+                                <?php foreach ($exercices as $exercice): ?>
+                                    <option value="<?= $exercice['id']?>"><?= $exercice['name']?></option>
+                                <?php endforeach; ?>
                             </select>
                         </div>
 
-                        <input type="submit" class="btn btn-primary rounded col-12" value="Choisir ce cours">
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="language"></label>
+                            <select class="form-select" id="language">
+                                <option selected>Choisir un language...</option>
+                                <option value="C">C</option>
+                                <option value="Python">Python</option>
+                            </select>
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <input type="file" name="file" class="form-control" id="file">
+                        </div>
+
+                        <input type="submit" class="btn btn-primary rounded col-12" value="Soumettre">
                     </form>
+                <?php else: ?>
+                    <div class="d-flex align-items-center text-danger gap-2">
+                        <i class="bi bi-exclamation-triangle-fill"></i>
+                        <p class="m-0">Problème lors du chargement des exercices.</p>
+                    </div>
                 <?php endif; ?>
-            <?php else: ?>
-                <div class="d-flex align-items-center text-warning gap-2">
-                    <i class="bi bi-exclamation-triangle-fill"></i>
-                    <p class="m-0">Problème lors du chargement des cours.</p>
-                </div>
-            <?php endif; ?>
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
 </div>
 
 <?php
