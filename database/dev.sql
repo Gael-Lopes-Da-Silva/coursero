00fb5dd9d04ef1caf48f11c7f74ef577de88a056
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-04-07 11:07:23 +0200

    feat: added a dev sql script

---
+++ database/dev.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'teacher', 'student') DEFAULT "student"
+);
+
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE exercises (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    course_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    reference_file VARCHAR(255) NOT NULL,
+    args JSON NOT NULL DEFAULT '[]',
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+);
+
+CREATE TABLE submissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    course_id INT NOT NULL,
+    exercise_id INT NOT NULL,
+    language VARCHAR(50) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    status ENUM('pending', 'running', 'done') NOT NULL,
+    score INT,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id)
+);
